49b6d6e1292b283e58b8390c5a1172fd70b15395
Abdullah Abou Mahmoud <aaboumahmoud@microsoft.com>
2025-03-21 01:57:17 -0400

    update auth

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
 
 class DefaultConfig:
-    """Bot Configuration"""
-    PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    APP_TYPE = os.environ.get("MicrosoftAppType", "UserAssignedMSI")
-    USER_ASSIGNED_CLIENT_ID = os.environ.get("AZURE_CLIENT_ID", "")
+    PORT = int(os.environ.get("PORT", 3978))
+    MicrosoftAppId = os.environ.get("MicrosoftAppId", "")
+    MicrosoftAppType = os.environ.get("MicrosoftAppType", "UserAssignedMSI")
+    MicrosoftAppTenantId = os.environ.get("MicrosoftAppTenantId", "")
+    MicrosoftAppClientId = os.environ.get("AZURE_CLIENT_ID", "")
