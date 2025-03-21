cd4425f2c234cc173d7a542b82e9a31adc5e4fba
Abdullah Abou Mahmoud <aaboumahmoud@microsoft.com>
2025-03-21 09:41:07 -0400

    convincing

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class DefaultConfig:
-    MicrosoftAppId = os.environ.get("MicrosoftAppId", "")  # Your bot's App Registration ID
+    MicrosoftAppId = os.environ.get("AZURE_CLIENT_ID", "")  # Your bot's App Registration ID
     MicrosoftAppType = os.environ.get("MicrosoftAppType", "UserAssignedMSI")
     MicrosoftAppTenantId = os.environ.get("MicrosoftAppTenantId", "")
     MicrosoftAppClientId = os.environ.get("AZURE_CLIENT_ID", "")  # UAMI Client ID
